af92585c6f022908b989670062ec25cd7219dda7
Premanshu Kashyap <kxprem29@gmail.com>
2026-01-20 19:19:12 +0530

    Mapping in JPA, JoinColumn, OwningSide & Inverse Side

---
+++ src/main/java/com/kxprem/hospitalManagement/entity/Appointment.java
@@ -0,0 +1,34 @@
+package com.kxprem.hospitalManagement.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+public class Appointment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDateTime appointmentTime;
+
+    @Column(length = 500)
+    private String reason;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Patient patient;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Doctor doctor;
+}
---
+++ src/main/java/com/kxprem/hospitalManagement/entity/Department.java
@@ -0,0 +1,34 @@
+package com.kxprem.hospitalManagement.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.print.Doc;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String name;
+
+    @OneToOne
+    private Doctor headDoctor;
+
+    @ManyToMany
+//    @JoinTable
+    private Set<Doctor> doctors =new HashSet<>();
+}
